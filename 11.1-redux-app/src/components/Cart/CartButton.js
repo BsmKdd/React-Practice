27471afd24a6ebef1f5ba9c715a5af20810a5f59
bassam kaddoura <BsmKdd@outlook.com>
2021-07-10 21:16:46 +0200

    redux #2

--- 11.1-redux-app/src/components/Cart/CartButton.js
+++ 11.1-redux-app/src/components/Cart/CartButton.js
@@ -1,12 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 
+import { uiActions } from '../../store/ui-slice';
+
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+	const dispatch = useDispatch();
+	const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+	const toggleCartHandler = () => {
+		dispatch(uiActions.toggleCart());
+	};
+
+	return (
+		<button onClick={toggleCartHandler} className={classes.button}>
+			<span>My Cart</span>
+			<span className={classes.badge}>{totalQuantity}</span>
+		</button>
+	);
 };
 
 export default CartButton;
